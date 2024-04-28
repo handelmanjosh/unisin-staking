0729b686d50d2240db6258126fdf88086f7a6b10
josh handelman <handelmanjos@gmail.com>
2024-04-27 23:57:02 -0400

    stuff

--- anchor/programs/unisin-staking/src/lib.rs
+++ anchor/programs/unisin-staking/src/lib.rs
@@ -1,4 +1,5 @@
 use anchor_lang::prelude::*;
+use anchor_spl::token::{mint_to, MintTo, Mint, TokenAccount, Token, transfer, Transfer};
 
 declare_id!("7zk7rrBgXsCT7ZTz241BfZh3y8mkvLYscrh9rBmq85Yv");
 
@@ -6,11 +7,209 @@ declare_id!("7zk7rrBgXsCT7ZTz241BfZh3y8mkvLYscrh9rBmq85Yv");
 pub mod unisin_staking {
     use super::*;
 
-    pub fn greet(_ctx: Context<Initialize>) -> Result<()> {
-        msg!("GM!");
+    pub fn initialize(_ctx: Context<Initialize>) -> Result<()> {
+        // msg!("Token state initialized");
+        Ok(())
+    }
+
+    pub fn stake(ctx: Context<Stake>) -> Result<()> {
+        // make pda for nft collection
+        // transfer nft to pda
+        // make sure that nft is part of valid collection
+        transfer(
+            CpiContext::new(
+                ctx.accounts.token_program.to_account_info(),
+                Transfer {
+                    from: ctx.accounts.nft_account.to_account_info(),
+                    to: ctx.accounts.stake_token_account.to_account_info(),
+                    authority: ctx.accounts.user.to_account_info(),
+                }
+            ),
+            1
+        )?; // remember to add error handling
+        let staking_account = &mut ctx.accounts.stake_account;
+        staking_account.staked_time = Clock::get()?.unix_timestamp;
+        staking_account.owner = ctx.accounts.user.key();
+        staking_account.mint = ctx.accounts.nft_account.mint;
+        Ok(())
+    }
+    pub fn unstake(ctx: Context<Unstake>) -> Result<()> {
+        // transfer nft from pda
+        // close pda
+        transfer(
+            CpiContext::new_with_signer(
+                ctx.accounts.token_program.to_account_info(),
+                Transfer {
+                    from: ctx.accounts.stake_token_account.to_account_info(),
+                    to: ctx.accounts.nft_account.to_account_info(),
+                    authority: ctx.accounts.program_authority.to_account_info(),
+                },
+                &[&[b"auth", &[ctx.bumps.program_authority]]]
+            ),
+            1
+        )?;
+        let time_diff = Clock::get()?.unix_timestamp - ctx.accounts.stake_account.staked_time;
+        let tokens = time_diff as u64; //how do we calculate tokens earned?
+        mint_to(
+            CpiContext::new_with_signer(
+                ctx.accounts.token_program.to_account_info(),
+                MintTo {
+                    to: ctx.accounts.user_token_account.to_account_info(),
+                    mint: ctx.accounts.token_mint.to_account_info(),
+                    authority: ctx.accounts.token_mint.to_account_info(),
+                },
+                &[&[b"mint", &[ctx.bumps.token_mint]]]
+            ),
+            tokens
+        )?;
+        Ok(())
+    }
+    pub fn claim(ctx: Context<Claim>) -> Result<()> {
+        let timestamp = Clock::get()?.unix_timestamp;
+        let diff = timestamp - ctx.accounts.stake_account.staked_time;
+        let tokens = diff as u64;
+        mint_to(
+            CpiContext::new_with_signer(
+                ctx.accounts.token_program.to_account_info(),
+                MintTo {
+                    to: ctx.accounts.user_token_account.to_account_info(),
+                    mint: ctx.accounts.token_mint.to_account_info(),
+                    authority: ctx.accounts.token_mint.to_account_info()
+                },
+                &[&[b"mint", &[ctx.bumps.token_mint]]]
+            ),
+            tokens,
+        )?;
+        ctx.accounts.stake_account.staked_time = timestamp;
         Ok(())
     }
 }
 
 #[derive(Accounts)]
-pub struct Initialize {}
+pub struct Initialize<'info> {
+    #[account(
+        init,
+        seeds = [b"mint"],
+        bump,
+        payer = user,
+        mint::decimals = 6,
+        mint::authority = mint,
+    )]
+    pub mint: Account<'info, Mint>,
+    #[account(
+        init,
+        seeds = [b"auth"],
+        bump,
+        payer = user,
+        space = 8
+    )]
+    /// CHECK: fuck off
+    pub program_authority: UncheckedAccount<'info>,
+    #[account(mut)]
+    pub user: Signer<'info>,
+    pub token_program: Program<'info, Token>,
+    pub rent: Sysvar<'info, Rent>,
+    pub system_program: Program<'info, System>,
+}
+
+#[account]
+pub struct StakeInfo {
+    owner: Pubkey,
+    mint: Pubkey,
+    staked_time: i64,
+}
+#[derive(Accounts)]
+pub struct Stake<'info> {
+    #[account(
+        init,
+        seeds = [b"stake", user.key().as_ref(), nft_account.key().as_ref()],
+        bump,
+        payer = user,
+        space = 8 + 32 + 32 + 8,
+    )]
+    pub stake_account: Account<'info, StakeInfo>,
+    #[account(
+        init,
+        seeds = [b"stake_account", user.key().as_ref(), nft_account.key().as_ref()],
+        bump,
+        payer = user,
+        token::mint = mint,
+        token::authority = program_authority,
+    )]
+    pub stake_token_account: Account<'info, TokenAccount>,
+    #[account(mut)]
+    pub user: Signer<'info>,
+    #[account(mut)]
+    pub nft_account: Account<'info, TokenAccount>,
+    pub mint: Account<'info, Mint>,
+    pub system_program: Program<'info, System>,
+    #[account(
+        seeds = [b"auth"],
+        bump
+    )]
+    /// CHECK: fuck off
+    pub program_authority: UncheckedAccount<'info>,
+    pub token_program: Program<'info, Token>,
+}
+
+#[derive(Accounts)]
+pub struct Unstake<'info> {
+    #[account(
+        mut,
+        seeds = [b"stake", user.key().as_ref(), nft_account.key().as_ref()],
+        bump,
+        close = user,
+    )]
+    pub stake_account: Account<'info, StakeInfo>,
+    #[account(
+        mut,
+        seeds = [b"stake_account", user.key().as_ref(), nft_account.key().as_ref()],
+        bump,
+    )]
+    pub stake_token_account: Account<'info, TokenAccount>,
+    #[account(mut)]
+    pub user: Signer<'info>,
+    #[account(mut)]
+    pub nft_account: Account<'info, TokenAccount>,
+    #[account(mut)]
+    pub user_token_account: Account<'info, TokenAccount>,
+    pub system_program: Program<'info, System>,
+    pub token_program: Program<'info, Token>,
+    #[account(
+        mut,
+        seeds = [b"mint"],
+        bump,
+    )]
+    pub token_mint: Account<'info, Mint>,
+    #[account(
+        seeds = [b"auth"],
+        bump,
+    )]
+    /// CHECK: fuck off
+    pub program_authority: AccountInfo<'info>,
+}
+
+
+#[derive(Accounts)]
+pub struct Claim<'info> {
+    #[account(
+        mut,
+        seeds = [b"stake", user.key().as_ref(), nft_account.key().as_ref()],
+        bump,
+    )]
+    pub stake_account: Account<'info, StakeInfo>,
+    #[account(mut)]
+    pub nft_account: Account<'info, TokenAccount>,
+    #[account(mut)]
+    pub user: Signer<'info>,
+    #[account(mut)]
+    pub user_token_account: Account<'info, TokenAccount>,
+    #[account(
+        mut,
+        seeds = [b"mint"],
+        bump
+    )]
+    pub token_mint: Account<'info, Mint>,
+    pub system_program: Program<'info, System>,
+    pub token_program: Program<'info, Token>
+}
--- anchor/tests/unisin-staking.spec.ts
+++ anchor/tests/unisin-staking.spec.ts
@@ -1,16 +1,179 @@
 import * as anchor from '@coral-xyz/anchor';
 import { Program } from '@coral-xyz/anchor';
 import { UnisinStaking } from '../target/types/unisin_staking';
-
-describe('unisin-staking', () => {
+import { TOKEN_PROGRAM_ID, createMint, getOrCreateAssociatedTokenAccount, mintTo, getAccount } from "@solana/spl-token";
+import { assert } from "chai";
+describe("nft-staking", () => {
   // Configure the client to use the local cluster.
-  anchor.setProvider(anchor.AnchorProvider.env());
+  const provider = anchor.AnchorProvider.env();
+  anchor.setProvider(provider);
+  const wallet = provider.wallet as anchor.Wallet;
+  const program = anchor.workspace.NftStaking as Program<UnisinStaking>;
 
-  const program = anchor.workspace.UnisinStaking as Program<UnisinStaking>;
-
-  it('should run the program', async () => {
+  const [mint] = anchor.web3.PublicKey.findProgramAddressSync(
+    [Buffer.from("mint")],
+    program.programId,
+  );
+  const [programAuthority] = anchor.web3.PublicKey.findProgramAddressSync(
+    [Buffer.from("auth")],
+    program.programId,
+  );
+  const initialize = async () => {
+    const tx = await program.methods.initialize().accounts({
+      mint,
+      programAuthority,
+    }).rpc();
+  }
+  it("Is initialized!", async () => {
     // Add your test here.
-    const tx = await program.methods.greet().rpc();
-    console.log('Your transaction signature', tx);
+    await initialize();
+  });
+  
+  const stake = async () => {
+    const nftMint = await createMint(
+      provider.connection,
+      wallet.payer,
+      wallet.publicKey,
+      null,
+      0
+    );
+    const nftAccount = await getOrCreateAssociatedTokenAccount(
+      provider.connection,
+      wallet.payer,
+      nftMint,
+      wallet.publicKey,
+    );
+    await mintTo(
+      provider.connection,
+      wallet.payer,
+      nftMint,
+      nftAccount.address,
+      wallet.payer,
+      1
+    );
+    const [stakeAccount] = anchor.web3.PublicKey.findProgramAddressSync(
+      [Buffer.from("stake"), wallet.publicKey.toBuffer(), nftAccount.address.toBuffer()],
+      program.programId,
+    );
+    const [stakeTokenAccount] = anchor.web3.PublicKey.findProgramAddressSync(
+      [Buffer.from("stake_account"), wallet.publicKey.toBuffer(), nftAccount.address.toBuffer()],
+      program.programId,
+    )
+    await program.methods.stake().accounts({
+      stakeAccount,
+      stakeTokenAccount,
+      user: wallet.publicKey,
+      mint: nftMint,
+      programAuthority,
+      nftAccount: nftAccount.address,
+      tokenProgram: TOKEN_PROGRAM_ID,
+    }).signers([wallet.payer]).rpc();
+    const fetched = await program.account.stakeInfo.fetch(stakeAccount);
+
+    assert(fetched.mint.toString() == nftMint.toString());
+    assert(fetched.owner.toString() == wallet.publicKey.toString());
+    assert(fetched.stakedTime.toNumber() > 0);
+    const stakedTokenAccount = await getAccount(
+      provider.connection,
+      stakeTokenAccount
+    );
+    assert(stakedTokenAccount.amount == BigInt(1), "Token account does not contain token");
+    let nft = await getOrCreateAssociatedTokenAccount(
+      provider.connection,
+      wallet.payer,
+      nftMint,
+      wallet.publicKey,
+    );
+    assert(nft.amount == BigInt(0), "Token not transferred");
+    return { nftMint, nftAccount, stakeTokenAccount, stakeAccount }
+  }
+  it("can stake nft", async () => {
+    // create nft collection for testing
+    await stake();
+  });
+  it("can claim rewards", async () => {
+    const { nftMint, nftAccount, stakeTokenAccount, stakeAccount } = await stake();
+    const stakeAccountBefore = await program.account.stakeInfo.fetch(stakeAccount);
+    const userTokenAccount = await getOrCreateAssociatedTokenAccount(
+      provider.connection,
+      wallet.payer,
+      mint,
+      wallet.publicKey,
+    );
+    await program.methods.claim().accounts({
+      stakeAccount,
+      user: wallet.publicKey,
+      userTokenAccount: userTokenAccount.address,
+      nftAccount: nftAccount.address,
+      tokenMint: mint,
+    }).signers([wallet.payer]).rpc();
+    const nft = await getOrCreateAssociatedTokenAccount(
+      provider.connection,
+      wallet.payer,
+      nftMint,
+      wallet.publicKey,
+    );
+    assert(nft.amount == BigInt(0), "User withdrew nft");
+    const nftStakeAccount = await getAccount(
+      provider.connection,
+      stakeTokenAccount,
+    );
+    assert(nftStakeAccount.amount == BigInt(1), "NFT stake account lost nft");
+    const userTokens = await getOrCreateAssociatedTokenAccount(
+      provider.connection,
+      wallet.payer,
+      mint,
+      wallet.publicKey,
+    );
+    assert(userTokens.amount > BigInt(0), "User did not get any tokens");
+    const stakeAccountAfter = await program.account.stakeInfo.fetch(stakeAccount);
+    assert(stakeAccountAfter.stakedTime > stakeAccountBefore.stakedTime, "Staked time not updated");
+  })
+  it("can unstake nft, getting tokens", async () => {
+    const { nftMint, stakeTokenAccount, stakeAccount } = await stake();
+    const userTokenAccount = await getOrCreateAssociatedTokenAccount(
+      provider.connection,
+      wallet.payer,
+      mint,
+      wallet.publicKey,
+    );
+
+    const nftAccount = await getOrCreateAssociatedTokenAccount(
+      provider.connection,
+      wallet.payer,
+      nftMint,
+      wallet.publicKey,
+    );
+    await program.methods.unstake().accounts({
+      stakeAccount,
+      stakeTokenAccount,
+      tokenMint: mint,
+      user: wallet.publicKey,
+      nftAccount: nftAccount.address,
+      userTokenAccount: userTokenAccount.address,
+      programAuthority,
+    }).rpc();
+
+    const stakedAccountAfter = await getAccount(
+      provider.connection,
+      stakeTokenAccount,
+    );
+    assert(stakedAccountAfter.amount == BigInt(0), "NFT not transferred to user");
+    let nft = await getOrCreateAssociatedTokenAccount(
+      provider.connection,
+      wallet.payer,
+      nftMint,
+      wallet.publicKey,
+    );
+    assert(nft.amount == BigInt(1), "User does not have nft");
+
+    let token = await getOrCreateAssociatedTokenAccount(
+      provider.connection,
+      wallet.payer,
+      mint,
+      wallet.publicKey,
+    )
+    assert(token.amount > BigInt(0), "User did not get any tokens")
   });
 });
+
